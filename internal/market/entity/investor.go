515d6a02425d189b9f882d6e25a6e824acf9b89f
MariaGabrielaReis <mariagabrielagreis@gmail.com>
2025-02-24 09:17:17 -0300

    feat: create investor entity

---
+++ internal/market/entity/investor.go
@@ -0,0 +1,52 @@
+package entity
+
+type InvestorAssetPosition struct {
+	AssetID string
+	Shares  int
+}
+
+type Investor struct {
+	ID            string
+	Name          string
+	AssetPosition []*InvestorAssetPosition
+}
+
+func NewInvestorAssetPosition(assetID string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetID: assetID,
+		Shares:  shares,
+	}
+}
+
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition) {
+	i.AssetPosition = append(i.AssetPosition, assetPosition)
+}
+
+func (i *Investor) AdjustAssetPosition(assetID string, quantityShares int) {
+	assetPosition := i.GetAssetPosition(assetID)
+	if assetPosition == nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosition(assetID, quantityShares))
+	} else {
+		assetPosition.AddShares(quantityShares)
+	}
+}
+
+func (i *Investor) GetAssetPosition(assetID string) *InvestorAssetPosition {
+	for _, assetPosition := range i.AssetPosition {
+		if assetPosition.AssetID == assetID {
+			return assetPosition
+		}
+	}
+	return nil
+}
+
+func (iap *InvestorAssetPosition) AddShares(quantity int) {
+	iap.Shares += quantity
+}
